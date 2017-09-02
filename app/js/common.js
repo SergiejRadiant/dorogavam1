58a04966dbbeefdeefb886ff8872c34f920426c0
Murawski <Murawski>
2017-09-02 17:27:25 +0300

    docs

--- app/js/common.js
+++ app/js/common.js
@@ -1,29 +1,19 @@
 $(function() {
 
-	!function (e, t, n) {
-   "use strict";
-    var r = e.querySelectorAll(".menu"), a = function (e, t) {
-    var n = t.getAttribute("aria-hidden");
-    "false" == n ? (e.setAttribute("aria-expanded", "false"), 
-		            t.setAttribute("aria-hidden", "true") ) : 
-		           (e.setAttribute("aria-expanded", "true"), 
-		            t.setAttribute("aria-hidden", "false")
-					);
-    };
-    [].forEach.call(r, function (e, t) {
-    var n = e.nextElementSibling;
-            e.setAttribute("aria-expanded", "false"), 
-		    e.setAttribute("aria-controls", "button-menu-" + t), 
-            n.setAttribute("aria-hidden", "true"), 
-			n.setAttribute("aria-labelledby", "content-menu-" + t),
-			n.setAttribute("tabindex", "0"), 
-		e.addEventListener("click", function () {
-            return a(this, n), !1;
-        }, !1), e.addEventListener("keydown", function (e) {
-            32 === e.which && (e.preventDefault(), a(this, n));
-        }, !1);
-    });
-	}(document, window);
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom',
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
